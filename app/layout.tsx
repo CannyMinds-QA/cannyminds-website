9e362b1e91a565ab2a283cdbd649ff014df192ea
Premkumar-CM <prem@cannymindstech.com>
2026-02-10 14:00:50 +0530

    feat: added ahref for demo

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <meta name="theme-color" content="#3170b5" />
+        <meta name="ahrefs-site-verification" content="db6974984581a64ee51e9fcc01d6fd2811d8665dad430ca8e1a355a43372b806" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       </head>
