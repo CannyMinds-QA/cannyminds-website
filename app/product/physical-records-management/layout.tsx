3436a4a0914ce8ab7d19867d2eb91cb9512c6dd6
Premkumar-CM <prem@cannymindstech.com>
2026-03-27 10:35:10 +0530

    fix: schema

--- app/product/physical-records-management/layout.tsx
+++ app/product/physical-records-management/layout.tsx
@@ -75,6 +75,8 @@ const jsonLd = {
             },
             "offers": {
                 "@type": "Offer",
+                "price": "0",
+                "priceCurrency": "INR",
                 "priceSpecification": {
                     "@type": "UnitPriceSpecification",
                     "priceType": "https://schema.org/InvoicePrice",
